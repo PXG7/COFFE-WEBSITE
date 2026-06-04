e004ce536f6ac4a7dfc0044bf9c91cfb57d3a87e
Pokhrelgokul <pokhrelgokul2@gmail.com>
2026-06-05 01:23:36 +0545

    dir update

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ document.body.classList.toggle("show-mobile-menu");
 menuCloseButton.addEventListener("click", () => {
 document.body.classList.remove("show-mobile-menu");
 });
+// Initialize swiper//
+const swiper = new Swiper('.swiper', {
+ 
+  loop: true,
+  grabCursor:true,
+  spaceBetween:25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+//Responsive breakpoints
+  breakpoints: {
+    0:{
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+    }
+});
